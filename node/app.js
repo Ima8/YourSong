119f3234b3aded19a38ada7c5e35004456476300
Issaret Prachitmutita <57130500097@st.sit.kmutt.ac.th>
2015-01-18 13:29:05 +0700

    add new song

--- node/app.js
+++ node/app.js
@@ -1,60 +1,168 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('express')();
+var fs = require('fs');
+var youtube = require("youtube-feeds");
+var obj;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+console.log("server start");
+var name = {};
+var roommodel = {
+  "roomId": "1",
+  "owner": "123",
+  "pass": null,
+  "playnow": "P1g99XOn5VY",
+  "Vtime": "20",
+  "playlist": [""]
+};
+var countData = 0;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+io.sockets.on('connection', function(socket) {
 
-var app = express();
+  ///
+  socket.on('sRoom', function(data) {
+    var roomId = data;
+    var roomHas = false;
+    console.log(data);
+    fs.readFile('json/listroom.json', function(err, data) {
+      if (err) {
+        console.error(err);
+      } else {
+        var json = JSON.parse(data);
+        console.log("ListROOM : " + json.length);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+        et1_checkRoom(function() {
+          et2_addNewRoom();
         });
-    });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+        function et1_checkRoom(callback) {
+          for (var i = 0; i < json.length; i++) {
+            // had room
+            if (json[i].roomId == roomId) {
+              console.log("Had ROOM : " + roomId);
+              json[i].pass = null;
+              socket.emit("sRoom", json[i]);
+              roomHas = true;
+              socket.room = roomId;
+              socket.join(roomId);
+              console.log(socket.room);
+              //socket.to("1").emit("cRoom", "joined ");
+            }
+          }
+          callback();
+        }
+
+        function et2_addNewRoom() {
+          if (roomHas == false) {
+            console.log(roommodel);
+            //var json2 = JSON.parse(roommodel);
+            var json2 = roommodel;
+            json2.roomId = roomId;
+            json2.owner = socket.id;
+            // save room id for this socket
+            socket.room = roomId;
+            socket.join(roomId);
+            json2.playnow = "xpFavEX4KN8";
+            json2.Vtime = "0";
+            json2.pass = null;
+            json[json.length] = json2;
+            console.log(json);
+            var strjson = JSON.stringify(json);
+            fs.writeFile('json/listroom.json', strjson, function(err) {
+              if (err) throw err;
+              // io.sockets.emit('playlist', strjson);
+              console.log('It\'s saved! >>> roomId = ' + roomId);
+            });
+          }
+        }
+
+      }
     });
+  });
+
+  socket.on('chowner', function(data) {
+    var pass = data;
+    fs.readFile('json/listroom.json', function(err, data) {
+      var json = JSON.parse(data);
+      for (var i = 0; i < json.length; i++) {
+        // had room
+        if (json[i].roomId == socket.room) {
+          if ((json[i].pass == pass)||(json[i].pass==null)) {
+            json[i].owner = socket.id;
+            fs.writeFile('json/listroom.json', JSON.stringify(json), function(err) {
+              if (err) throw err;
+              console.log(socket.id + " chowner : Save >> " + socket.room);
+            });
+          } else {
+            console.log(socket.id + " chowner : pass wrong >> " + socket.room);
+
+          }
+        }
+      }
+    });
+  });
+
+
+  /// newSong
+  socket.on('newSong', function(data) {
+    var youtubeId = data;
+    youtube.video(youtubeId, function(err, result) { // check vaild youtube id
+
+      if (result.id === youtubeId) {
+        fs.readFile('json/listroom.json', function(err, data) {
+          if (err) {
+            console.error(err);
+          } else {
+            var json = JSON.parse(data);
+            var newItem = youtubeId;
+            // json.playlist.splice(0,1);
+            for (var i = 0; i < json.length; i++) {
+              // had room
+              if ((json[i].roomId == socket.room) && (json[i].owner == socket.id)) {
+                json[i].playlist[json[i].playlist.length] = newItem;
+                console.log(json[i].playlist.length);
+                var strjson = JSON.stringify(json);
+                fs.writeFile('json/listroom.json', strjson, function(err) {
+                  if (err) throw err;
+                  // io.sockets.emit('playlist', strjson);
+                  console.log(socket.id + " newSong " + youtubeId + " : Save >> " + socket.room);
+
+                });
+                console.log(socket.id + " newSong : Room Ok & Owner Ok >> " + socket.room);
+              }
+            }
+          }
+        });
+      }
+    });
+    console.log(data);
+  });
+
+
+
+  socket.on('disconnect', function() {
+    console.log('Got disconnect! >>>> ' + socket.id);
+
+  });
+
+  function checkPermission(id) {
+    fs.readFile('json/listroom.json', function(err, data) {
+      for (var i = 0; i < json.length; i++) {
+        // had room
+        if (json[i].roomId == socket.room) {
+          if (json[i].owner == id) {
+            console.log(socket.id + " checkPermission : User OK >> " + socket.room);
+            return true;
+          } else {
+            console.log(socket.id + " checkPermission : User wrong >> " + socket.room);
+            return false;
+          }
+        }
+      }
+    });
+    return false;
+  }
+
+
+
 });
-
-
-module.exports = app;
+server.listen(9998);
